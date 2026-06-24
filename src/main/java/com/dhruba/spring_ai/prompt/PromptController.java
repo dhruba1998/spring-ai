74dabc98137688c617de583af4d5a22b0a32476f
Dhruba Roy <dhrubaroy1011@gmail.com>
2026-06-24 09:44:00 +0530

    Add todos

---
+++ src/main/java/com/dhruba/spring_ai/prompt/PromptController.java
@@ -0,0 +1,9 @@
+package com.dhruba.spring_ai.prompt;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PromptController {
+
+    //Implement prompt template,multi-modal input, chat memory
+}
